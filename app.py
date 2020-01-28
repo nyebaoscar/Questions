0917e25700656062314d7192a934ac2efeac38d5
nyebaoscar <nyebaoscar@gmail.com>
2020-01-28 23:45:51 +0300

    changes in db

--- app.py
+++ app.py
@@ -1,38 +1,94 @@
-from flask import flask
+from flask import Flask,render_template,g,request,session,redirect, url_for
+from database import get_db
+from werkzeug.security import generate_password_hash, check_password_hash
+import os
 
 app=Flask(__name__)
+app.config['SECRET_KEY'] = os.urandom(24)
+
+@app.teardown_appcontext
+def close_db(error):
+    if hasattr(g, 'sqlite_db'):
+        g.sqlite_db.close()
+        
+#function to return whether user exists or not#
+def get_current_user():
+    user_result = None
+    if 'user' in session:
+        user = session['user']   
+        
+        db = get_db()
+        user_cur = db.execute('select id, name, Password,expert,admin from users where name = ?', [user])
+        user_result = user_cur.fetchone()
+        
+    return user_result
 
 @app.route('/')
 def index():
-    pass
+    user = get_current_user()
+    return render_template('home.html', user=user)
 
-@app.route('/register')
+@app.route('/register',methods=['GET','POST'])
 def register():
-    pass
+    user = get_current_user()
+    if request.method == 'POST':
+        db = get_db()
+        hashed_password = generate_password_hash(request.form['password'], method='sha256')
+        db.execute('insert into users (name, password, expert, admin) values(?,?,?,?)',[request.form['name'], hashed_password, '0', '0'])
+        db.commit()
+        return '<h1>User Created</h1>'
+    
+    return render_template('register.html', user=user)
 
-@app.route('/login')
+@app.route('/login', methods=['GET','POST'])
 def login():
-    return render_template('login.html')
+    user = get_current_user()
+    if request.method == 'POST':
+        db = get_db()
+        
+        name = request.form['name']
+        password = request.form['password']
+        
+        user_cur = db.execute('select id, name, Password from users where name = ?', [name])
+        user_result = user_cur.fetchone()
+        
+        if check_password_hash(user_result['password'], password):
+            session['user'] = user_result['name']
+            return '<h1>The password is correct!</h1>'
+        else:
+            return '<h1>The password is incorrect!</h1>'
+    
+    return render_template('login.html', user=user)
 
 @app.route('/question')
 def question():
-    return render_template('question.html')
+    user = get_current_user()
+    return render_template('question.html', user=user)
 
 @app.route('/answer')
 def answer():
-    pass
+    user = get_current_user()  
+    return render_template('answer.html', user=user)
 
-@app.route('ask')
+@app.route('/ask')
 def ask():
-    pass
+    user = get_current_user()
+    return render_template('ask.html', user=user)
 
 @app.route('/unanswered')
 def unanswered():
-    pass
+    user = get_current_user()  
+    return render_template('unanswered.html', user=user)
 
 @app.route('/users')
 def users():
-    pass
+    user = get_current_user()   
+    return render_template('users.html', user=user)
+
+@app.route('/logout')
+def logout():
+    session.pop('user', None)
+    return redirect(url_for('index'))
 
 if __name__== '__main__':
     app.run(debug=True)
