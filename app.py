dbeb3eb6daf3c65dd08c534df096408dcad28773
nyebaoscar <nyebaoscar@gmail.com>
2020-02-03 15:20:34 +0300

    prevent duplicate users

--- app.py
+++ app.py
@@ -27,10 +27,11 @@ def get_current_user():
 def index():
     user = get_current_user()
     db = get_db()
-    
-    questions_cur = db.execute('select questions.id as question_id, questions.question_text, askers.name as asker_name,experts.name as expert_name from questions join users as askers on askers.id=questions.asked_by_id join users as experts on expert_id=questions.expert_id where questions.answer_text = null')
-    questions_results=questions_cur.fetchall()
-    return render_template('home.html', user=user, question=questions_results)
+
+    questions_cur = db.execute('select questions.id as question_id, questions.question_text, askers.name as asker_name, experts.name as expert_name from questions join users as askers on askers.id = questions.asked_by_id join users as experts on experts.id = questions.expert_id where questions.answer_text is not null')
+    questions_result = questions_cur.fetchall()
+
+    return render_template('home.html', user=user, questions=questions_result)
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
@@ -39,6 +40,12 @@ def register():
     if request.method == 'POST':
 
         db = get_db()
+        existing_user_cur = db.execute('select id from users where name = ?', [request.form['name']])
+        existing_user = existing_user_cur.fetchone()
+
+        if existing_user:
+            return render_template('register.html', user=user, error='User already exists!')
+
         hashed_password = generate_password_hash(request.form['password'], method='sha256')
         db.execute('insert into users (name, password, expert, admin) values (?, ?, ?, ?)', [request.form['name'], hashed_password, '0', '0'])
         db.commit()
@@ -71,28 +78,29 @@ def login():
     return render_template('login.html', user=user)
 
 @app.route('/question/<question_id>')
-def question():
+def question(question_id):
     user = get_current_user()
     db = get_db()
-    
-    question_cur = db.execute('select  questions.question_text, askers.name as asker_name,experts.name as expert_name from questions join users as askers on askers.id=questions.asked_by_id join users as experts on experts_id=questions.expert_id where questions.id=?',[question_id])
+
+    question_cur = db.execute('select questions.question_text, questions.answer_text, askers.name as asker_name, experts.name as expert_name from questions join users as askers on askers.id = questions.asked_by_id join users as experts on experts.id = questions.expert_id where questions.id = ?', [question_id])
     question = question_cur.fetchone()
-    
+
     return render_template('question.html', user=user, question=question)
 
-@app.route('/answer/<question_id>', methods=['GET','POST'])
+@app.route('/answer/<question_id>', methods=['GET', 'POST'])
 def answer(question_id):
     user = get_current_user()
     db = get_db()
-    
+
     if request.method == 'POST':
-        db.execute('update questions set answer_text=? where id=?', [request.form['answer'], question_id])
+        db.execute('update questions set answer_text = ? where id = ?', [request.form['answer'], question_id])
         db.commit()
+
         return redirect(url_for('unanswered'))
-        
-    
+
     question_cur = db.execute('select id, question_text from questions where id = ?', [question_id])
     question = question_cur.fetchone()
+
     return render_template('answer.html', user=user, question=question)
 
 @app.route('/ask', methods=['GET', 'POST'])
