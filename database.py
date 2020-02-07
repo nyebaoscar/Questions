31f9ec566786bb6ab6d6109c141cab406df35dcd
nyebaoscar <nyebaoscar@gmail.com>
2020-02-07 09:25:36 +0300

    change db to postgres

--- database.py
+++ database.py
@@ -1,13 +1,21 @@
 from flask import g
 import sqlite3
+import psycopg2
+from psycopg2.extras import DictCursor
 
 def connect_db():
-    sql = sqlite3.connect('G:\projects under construction\Questions/questions.db')
-    sql.row_factory = sqlite3.Row
-    return sql
+    conn = psycopg2.connect('postgres://mklcnjiyeceaao:25dc0a9db08d8dd6de52889d3525ff6ec944a1f29f1c00ef215c29a9981051f9@ec2-34-235-108-68.compute-1.amazonaws.com:5432/db75bvk6cnf71f', cursor_factory=DictCursor)
+    conn.autocommit = True
+    sql = conn.cursor()
+    return conn, sql
 
 def get_db():
-    if not hasattr(g, 'sqlite_db'):
-        g.sqlite_db = connect_db()
-    return g.sqlite_db
+    db = connect()
+    
+    if not hasattr(g, 'postgres_db_conn'):
+        g.postgres_db_conn = db[0]
+        
+    if not hasattr(g, 'postgres_db_cur'):
+        g.postgres_db_cur = db[1]
 
+    return g.postgres_db_cur
